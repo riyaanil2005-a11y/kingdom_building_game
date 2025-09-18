8e2a2c4a58c8b0475d5d6c3fbded7d307083bb8d
Riyaanil <riyaanil2005@gmail.com>
2025-09-18 20:46:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,11 @@
 import streamlit as st
 
-# Title for your app
 st.set_page_config(page_title="Kingdom Building Game", layout="wide")
 st.title("👑 Kingdom Building Game")
 
-st.markdown(
-    """
-    This is my browser-based game built with HTML, CSS, and JavaScript.
-    Below you can play it directly:
-    """,
-    unsafe_allow_html=True,
-)
+# Read your HTML file content
+with open("game.html", "r", encoding="utf-8") as f:
+    game_html = f.read()
 
-# Embed the game inside an iframe
-st.components.v1.html(
-    """
-    <iframe src="game.html" width="100%" height="600" style="border:none;"></iframe>
-    """,
-    height=650,
-)
+# Inject game directly
+st.components.v1.html(game_html, height=800, scrolling=True)
